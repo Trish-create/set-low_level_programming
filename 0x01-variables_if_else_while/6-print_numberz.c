e0336e9f50bd2246d99288746a30b82782f3c487
Trisher <trishmoyo303@gmail.com>
2026-02-25 00:30:00 +0200

    Implement number printing from 0 to 9
    This program prints all single digit numbers from 0 to 9 using putchar.

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		putchar(n + 48);
+		n++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
